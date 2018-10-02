25f38ec18b8b2d5178b84be830f7061f4079d164
praetiwa <42773877+kanngean@users.noreply.github.com>
2018-10-03 06:43:18 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,11 @@
+<html><head><title>PRAETIWA KANNGEAN</title></head>
+<meta http-equiv="content-type" content="text/html;charset=tis-620"/>
+<body bgcolor=pink>
+<table width=100% bgcolor=#006699><tr><td>
+<span style="font-size:30px;">WELCOME</span>
+</td></tr></table>
+<table width=100% bgcolor=#dddddd><tr>
+<td><a href=myweb.php>home</a></td>
+
+
+</tr></table>
